9088028dfd38f22b840151307faed08f71500acc
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-05-25 02:57:44 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Content from './componentes/Content';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Content />
+      </BrowserRouter>
+    );
   }
 }
 
 export default App;
-// iniciando projeto
---
+++ src/componentes/Content.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Album from '../pages/Album';
+import Favorites from '../pages/Favorites';
+import Login from '../pages/Login';
+import NotFound from '../pages/NotFound';
+import Profile from '../pages/Profile';
+import ProfileEdit from '../pages/ProfileEdit';
+import Search from '../pages/Search';
+
+class Content extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact path="" component={ NotFound } />
+      </Switch>
+    );
+  }
+}
+
+export default Content;
